288f3ca5c852c7e4715f2052b9374b166cc21daf
K.Adam White <kadamwhite@gmail.com>
2013-09-14 01:10:04 -0400

    Hide header banner on single artwork posts
    When the user clicks through to a specific work, that work shows up in the banner if it has been set as the featured image.
    It is (a) weird to be showing it to them twice, (b) sub-optimal to be cramming it into the narrow space at the top of the page, and (c) very bizarre to show them a *different* image before the one they asked for (as happens when a banner is set but the featured image is not).
    
    To keep the focus on the main artwork presentation, this prevents the header banner from rendering on single artwork pages.

--- wp-content/themes/emilygarfield/header.php
+++ wp-content/themes/emilygarfield/header.php
@@ -92,12 +92,7 @@
                 <?php
                     // The header image
                     // Check if this is a post or page, if it has a thumbnail, and if it's a big one
-                    if ( is_singular() && has_post_thumbnail( $post->ID ) &&
-                            ( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_width ) ) ) &&
-                            $image[1] >= $header_image_width ) :
-                        // Houston, we have a new header image!
-                        echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-                    else :
+                    if ( ! ( is_singular() && get_post_type() == 'ag_artwork_item' ) ) :
                         // Compatibility with versions of WordPress prior to 3.4.
                         if ( function_exists( 'get_custom_header' ) ) {
                             $header_image_width  = get_custom_header()->width;
