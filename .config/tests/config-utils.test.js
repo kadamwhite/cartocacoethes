6619f97002e224b5f740542c84de17f4a6e84a5b
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-02 20:38:49 -0400

    Update webpack config to use webpack-helpers

--- .config/tests/config-utils.test.js
+++
@@ -1,7 +0,0 @@
-import { filePath } from '../config-utils';
-
-test( 'properly generates a file system theme file path', () => {
-	expect( filePath() ).toBe( `${ process.cwd() }` );
-	expect( filePath( 'themes/ehg' ) ).toBe( `${ process.cwd() }/themes/ehg` );
-	expect( filePath( 'themes/ehg', 'build' ) ).toBe( `${ process.cwd() }/themes/ehg/build` );
-} );
--- .config/webpack.config.dev.js
+++ .config/webpack.config.dev.js
@@ -1,16 +1,8 @@
 /**
  * This file defines the configuration for development and dev-server builds.
  */
-const { join } = require( 'path' );
 const { externals, helpers, presets } = require( '@humanmade/webpack-helpers' );
-const { choosePort, cleanOnExit } = helpers;
-
-/**
- * Return the absolute file system path to a file within the content/ folder.
- * @param  {...String} relPaths Strings describing a file relative to the content/ folder.
- * @returns {String} An absolute file system path.
- */
-const filePath = ( ...relPaths ) => join( process.cwd(), ...relPaths );
+const { choosePort, cleanOnExit, filePath } = helpers;
 
 // Clean up manifests on exit.
 cleanOnExit( [
@@ -31,7 +23,7 @@ module.exports = choosePort( 9090 ).then( port => presets.development( {
 		editor: filePath( 'wp-content/themes/ehg/blocks/index.js' ),
 	},
 	output: {
-		path: filePath( 'wp-content/themes/ehg' ),
-		publicPath: `http://localhost:${ port }/wp-content/themes/ehg`,
+		path: filePath( 'wp-content/themes/ehg/build' ),
+		publicPath: `http://localhost:${ port }/wp-content/themes/ehg/build/`,
 	},
 } ) );
