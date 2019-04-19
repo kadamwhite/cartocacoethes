bc7beaae89f6e084cd4e2b30524fe51853b86689
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-19 14:36:46 -0400

    Suppress "missing React version" warning in ESLint

--- wp-content/themes/ehg2/.eslintrc.js
+++ wp-content/themes/ehg2/.eslintrc.js
@@ -3,6 +3,12 @@ module.exports = {
 	'extends': [
 		'humanmade',
 	],
+	'settings': {
+		// Tell eslint-plugin-react which version of React is in use. Current as of WordPress 5.1.
+		'react': {
+			'version': '16.6.3',
+		},
+	},
 	'rules': {
 		'react/react-in-jsx-scope': [ 'off' ],
 		'space-before-function-paren': [ 'error', {
