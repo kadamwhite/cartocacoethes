1f5d4f78a942eb330f86c3aa28045029164a7b7d
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-21 23:37:46 -0400

    Create top-level webpack config

--- .config/webpack.config.dev.js
+++ .config/webpack.config.dev.js
@@ -1,29 +1,38 @@
 /**
  * This file defines the configuration for development and dev-server builds.
  */
-const { externals, helpers, presets } = require( '@humanmade/webpack-helpers' );
+const { helpers, presets } = require( '@humanmade/webpack-helpers' );
 const { choosePort, cleanOnExit, filePath } = helpers;
 
-// Clean up manifests on exit.
-cleanOnExit( [
-	filePath( 'wp-content/themes/ehg/build/asset-manifest.json' ),
-] );
+const projects = [
+	'wp-content/plugins/artgallery',
+	// 'wp-content/plugins/featured-item-blocks',
+	'wp-content/themes/ehg2',
+];
 
-module.exports = choosePort( 9090 ).then( port => presets.development( {
-	devServer: {
-		port,
-	},
-	externals: {
-		...externals,
-		jquery: 'jQuery',
-	},
-	entry: {
-		customizer: filePath( 'wp-content/themes/ehg/src/customizer.js' ),
-		theme: filePath( 'wp-content/themes/ehg/src/index.js' ),
-		editor: filePath( 'wp-content/themes/ehg/src/editor.js' ),
-	},
-	output: {
-		path: filePath( 'wp-content/themes/ehg/build' ),
-		publicPath: `http://localhost:${ port }/wp-content/themes/ehg/build/`,
-	},
-} ) );
+// Clean up manifests on exit.
+cleanOnExit( projects.map( relPath => filePath( `${ relPath }/build/asset-manifest.json` ) ) );
+
+// Build array of partial config objects.
+const configs = projects.map( relPath => require( `../${ relPath }/.config/webpack.config.dev` ) );
+
+if ( process.argv[1].indexOf( 'webpack-dev-server' ) !== -1 ) {
+	// Webpack DevServer is loading this configuration: automatically detect & bind to an open port.
+	const cwdRelativePublicPath = ( path, port ) => `http://localhost:${ port }${ path.replace( process.cwd(), '' ) }/`;
+
+	module.exports = choosePort( 9090 ).then( port => {
+		return configs.map( config => presets.development( {
+			...config,
+			devServer: {
+				port,
+			},
+			output: {
+				...config.output,
+				publicPath: cwdRelativePublicPath( config.output.path, port ),
+			},
+		} ) );
+	} );
+} else {
+	// A dev-mode static file build is in progress.
+	module.exports = configs.map( config => presets.development( config ) );
+}
