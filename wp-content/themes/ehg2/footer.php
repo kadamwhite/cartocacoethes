b865e843aba68b48eae915214ea9d8525f581ae5
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-22 12:02:28 -0400

    Implement toggleable copyright notice in footer

--- wp-content/themes/ehg2/footer.php
+++ wp-content/themes/ehg2/footer.php
@@ -12,19 +12,7 @@
 ?>
 
 <footer id="colophon" class="site-footer">
-	<div class="site-info">
-		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ehg2' ) ); ?>">
-			<?php
-			/* translators: %s: CMS name, i.e. WordPress. */
-			printf( esc_html__( 'Proudly powered by %s', 'ehg2' ), 'WordPress' );
-			?>
-		</a>
-		<span class="sep"> | </span>
-		<?php
-			/* translators: 1: Theme name, 2: Theme author. */
-			printf( esc_html__( 'Theme: %1$s by %2$s.', 'ehg2' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>', 'the contributors' );
-		?>
-	</div><!-- .site-info -->
+	<?php get_template_part( 'template-parts/copyright-notice' ); ?>
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
