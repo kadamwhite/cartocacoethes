884f9c4cef4a1e3d801cf9bd682d7f2e5bbe1238
K. Adam White <kadamwhite@users.noreply.github.com>
2019-04-27 20:19:57 -0400

    Improve db restore script

--- .bin/import-production-db.sh
+++ .bin/import-production-db.sh
@@ -14,12 +14,21 @@ vagrant ssh -c '
 wp db reset --yes
 wp db import /vagrant/wp_emilygarfield.sql
 
+# Clean up database
 wp search-replace www.emilygarfield.com ehg.local
 wp search-replace emilygarfield.com ehg.local
 wp search-replace emilygarfield.wpengine.com ehg.local
 wp search-replace ehg.local/wp-content ehg.local/content
 
-wp plugin activate artgallery;
+# Setup plugins and theme
+wp plugin activate artgallery
 wp plugin activate featured-item-blocks
 wp theme activate cartocacoethes
+wp user create admin admin@ehg.local --role=administrator --user_pass=password
+
+# Update Artwork posts
+wp artgallery-migrate-acf-meta
+
+# Populate homepage with new content
+wp post update $( wp post list --name=homepage --post_type=page --field=ID ) --post_type=page --post_content="$( cat /vagrant/content/homepage-content.txt )" --post_title="Homepage"
 '
