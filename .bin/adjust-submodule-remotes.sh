8528eac379ad0b34cb8f85363f1e445483df5070
K. Adam White <kadamwhite@users.noreply.github.com>
2019-05-07 18:00:02 -0400

    Update adjust-submodule script so it can be used to take a fresh clone and make it usable

--- .bin/adjust-submodule-remotes.sh
+++ .bin/adjust-submodule-remotes.sh
@@ -1,23 +1,31 @@
+echo "Ensuring submodules are initialized"
+git submodule init
+git submodule update --recursive
+
 echo "Swapping 'cartocacoethes' remote from HTTP to SSH..."
-cd ../wp-content/themes/cartocacoethes
+cd wp-content/themes/cartocacoethes
 git remote rm origin
 git remote add origin git@github.com:kadamwhite/cartocacoethes.git
-cd -
+git fetch origin
+cd - > /dev/null
 
 echo "Swapping 'artgallery' remote from HTTP to SSH..."
-cd ../wp-content/plugins/artgallery
+cd wp-content/plugins/artgallery
 git remote rm origin
 git remote add origin git@github.com:kadamwhite/artgallery.git
-cd -
+git fetch origin
+cd - > /dev/null
 
 echo "Swapping 'featured-item-blocks' remote from HTTP to SSH..."
-cd ../wp-content/plugins/featured-item-blocks
+cd wp-content/plugins/featured-item-blocks
 git remote rm origin
 git remote add origin git@github.com:kadamwhite/featured-item-blocks.git
-cd -
+git fetch origin
+cd - > /dev/null
 
 echo "Swapping 'asset-loader' remote from HTTP to SSH..."
-cd ../wp-content/plugins/asset-loader
+cd wp-content/plugins/asset-loader
 git remote rm origin
 git remote add origin git@github.com:humanmade/asset-loader.git
-cd -
+git fetch origin
+cd - /dev/null
