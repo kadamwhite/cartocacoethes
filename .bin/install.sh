1f8990d382908cde0fac594d78e12cc4f4ece099
K. Adam White <kadamwhite@users.noreply.github.com>
2019-05-07 20:35:02 -0400

    Fix infinite loop in install script

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,39 +1,50 @@
+#!/usr/bin/env bash
+
 function setup-remotes() {
   git remote rm origin
   git remote add origin git@github.com:$1.git
   git fetch origin
 }
 
-function install() {
-  npm install
-  composer install
+function maybe-initialize-submodules() {
   git submodule init
   git submodule update --recursive
 }
 
 echo "Installing root project"
-install
+composer install
 
-echo "Swapping 'cartocacoethes' remote from HTTP to SSH..."
-cd wp-content/themes/cartocacoethes
+# echo "Swapping 'cartocacoethes' remote from HTTP to SSH..."
+cd wp-content/themes/cartocacoethes;
+echo $PWD;
 setup-remotes kadamwhite/cartocacoethes
-install
+maybe-initialize-submodules
+npm install
+composer install
 cd - > /dev/null
 
-echo "Swapping 'artgallery' remote from HTTP to SSH..."
-cd wp-content/plugins/artgallery
+# echo "Swapping 'artgallery' remote from HTTP to SSH..."
+cd wp-content/plugins/artgallery;
+echo $PWD;
 setup-remotes kadamwhite/artgallery
-install
+maybe-initialize-submodules
+npm install
+composer install
 cd - > /dev/null
 
-echo "Swapping 'featured-item-blocks' remote from HTTP to SSH..."
-cd wp-content/plugins/featured-item-blocks
+# echo "Swapping 'featured-item-blocks' remote from HTTP to SSH..."
+cd wp-content/plugins/featured-item-blocks;
+echo $PWD;
 setup-remotes kadamwhite/featured-item-blocks
-install
+maybe-initialize-submodules
+npm install
+composer install
 cd - > /dev/null
 
-echo "Swapping 'asset-loader' remote from HTTP to SSH..."
-cd wp-content/plugins/asset-loader
+# echo "Swapping 'asset-loader' remote from HTTP to SSH..."
+cd wp-content/plugins/asset-loader;
+echo $PWD;
 setup-remotes humanmade/asset-loader.git
-install
-cd - /dev/null
+# Very important: NO `npm install` in this directory!
+composer install
+cd - > /dev/null
