2cf9999a89057b594aa17edb637724ff8700dbc3
K. Adam White <kadamwhite@users.noreply.github.com>
2019-09-22 14:48:27 -0400

    Use HTTPS rel profile link

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php if ( ! ehg_is_amp() ) : ?>
 		<script>document.documentElement.classList.remove("no-js");</script>
